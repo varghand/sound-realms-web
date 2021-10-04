cf15044a5cef144e8ab305282d642a56b49088c5
Gustav Sundin <sundin666@gmail.com>
2021-10-04 18:21:22 +0200

    Add feedback page

---
+++ src/feedback/Feedback.vue
@@ -0,0 +1,28 @@
+<template>
+  <main-layout>
+    <div class=content>
+      <img alt="Sound Realms logo" class="logo" src="../assets/soundrealms.jpg">
+      <p>Survey coming soon...</p>
+      <p>
+        Meanwhile, feel free to get in touch with
+        <a href="mailto:app@sound-realms.com">app@sound-realms.com</a>.
+      </p>
+      <p>All thoughts, ideas, feedback and bug reports are very welcome!</p>
+    </div>
+  </main-layout>
+</template>
+
+<style scoped>
+.content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.logo {
+  padding: 12px;
+  max-width: 30%;
+  height: auto;
+  object-fit: scale-down;
+}
+</style>
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import Home from './home/Home.vue';
+import Feedback from './feedback/Feedback.vue';
 
 const routes = {
   '/': Home,
+  '/feedback': Feedback,
 };
 
 export default routes;
