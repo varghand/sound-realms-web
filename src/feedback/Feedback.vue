bd0009a394ed22fc39c4ad08af2d0238126134be
Gustav Sundin <sundin666@gmail.com>
2021-10-11 11:52:45 +0200

    Link to feedback survey

--- src/feedback/Feedback.vue
+++ src/feedback/Feedback.vue
@@ -2,9 +2,11 @@
   <main-layout>
     <div class=content>
       <img alt="Sound Realms logo" class="logo" src="../assets/soundrealms.jpg">
-      <p>Survey coming soon...</p>
+      <p>Follow this link to make your voice heard:
+        <a href="https://forms.gle/JNuwPujg5J3Kb4kp7">feedback survey</a>
+      </p>
       <p>
-        Meanwhile, feel free to get in touch with
+        Also feel free to get in touch with
         <a href="mailto:app@sound-realms.com">app@sound-realms.com</a>.
       </p>
       <p>All thoughts, ideas, feedback and bug reports are very welcome!</p>
