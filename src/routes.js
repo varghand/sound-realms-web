d338bb50db419496960df588329651423ef19726
Gustav Sundin <sundin666@gmail.com>
2021-11-09 17:38:28 +0100

    Add proper routes to all pages

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,15 @@
 import Home from './home/Home.vue';
 import Feedback from './feedback/Feedback.vue';
 import Gametest from './gametest/Gametest.vue';
+import Faq from './faq/Faq.vue';
+import Contact from './contact/Contact.vue';
 
 const routes = {
   '/': Home,
   '/feedback': Feedback,
-  '/faq': Feedback, // TODO
-  '/game-testing': Gametest, // TODO
-  '/contact': Feedback, // TODO
+  '/faq': Faq,
+  '/game-testing': Gametest,
+  '/contact': Contact,
 };
 
 export default routes;
