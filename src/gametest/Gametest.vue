19fb350bdf37e4691312df866a0f1f85a429b7c6
Gustav Sundin <sundin666@gmail.com>
2021-12-15 08:38:28 +0100

    New app store link

--- src/gametest/Gametest.vue
+++ src/gametest/Gametest.vue
@@ -19,7 +19,7 @@
           <a href="https://play.google.com/store/apps/details?id=com.wolfhand.audioquest">
           <img src="../assets/gametest/android_qr.png" class="qr"/>
           </a>
-          <a href="https://testflight.apple.com/join/ARR4pDmY">
+          <a href="https://apps.apple.com/se/app/sound-realms/id1587860372">
           <img src="../assets/gametest/ios_qr.png"  class="qr"/>
           </a>
         </div>
@@ -30,6 +30,9 @@
         <p>
           We will be super grateful if you take some time to answer the included
           <a href="https://forms.gle/JNuwPujg5J3Kb4kp7">feedback survey</a>. Feel free to contact us with any questions at <a href="mailto:dm@varghand.com">dm@varghand.com</a>.
+        </p>
+        <p>
+          Want to become a beta tester for upcoming releases? Send an email to: <a href="mailto:app@sound-realms.com">app@sound-realms.com</a>.
         </p>
       </div>
       <Footer/>
