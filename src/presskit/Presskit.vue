c6abc7d3fea144c1b1945aee2caac48c31aa4aec
Gustav Sundin <sundin666@gmail.com>
2022-03-15 08:34:58 +0100

    WIP: press kit

--- src/presskit/Presskit.vue
+++ src/presskit/Presskit.vue
@@ -6,7 +6,7 @@
         <div class="section-content">
           <h1>Sound Realms Press Kit</h1>
 
-          <p>This page contains content (text, images, logos, sample sound files and links) to use when featuring Sound Realms in any type of media. Feel free to get in touch at dm@varghand.com if there is anything else you need.</p>
+          <p>This page contains content (text, images, logos, sample sound files and links) to use when featuring Sound Realms in any type of media. Feel free to get in touch at <a href="mailto:dm@varghand.com">dm@varghand.com</a> if there is anything else you need.</p>
 
           <p><a href="/Sound_Realms_Press_Kit.zip">Download entire press kit as zip (10MB)</a></p>
 
@@ -17,6 +17,9 @@
 
           <p><a href="/faq">FAQ</a>: Explanations on Sound Realms, Mace & Magic, and more.</p>
           <p><a href="/learn-more">About us</a>: On the team and company behind Sound Realms.</p>
+
+          <h1>Image Gallery</h1>
+          <p>Press any image to open in full resolution.</p>
         </div>
       </section>
 
@@ -25,14 +28,15 @@
         <div class="section-content">
           <h1>Links</h1>
 
+          <p>Web page:<a href="https://www.sound-realms.com/">https://www.sound-realms.com/</a></p>
           <p>Kickstarter: <a href="https://www.kickstarter.com/projects/varghand/sound-realms-mace-and-magic" target="_blank">https://www.kickstarter.com/projects/varghand/sound-realms-mace-and-magic</a></p>
           <p>Download free demo: <a href="https://www.sound-realms.com/download" target="_blank">https://www.sound-realms.com/download</a></p>
           <p>Facebook: <a href="https://www.facebook.com/SoundRealms" target="_blank">https://www.facebook.com/SoundRealms</a></p>
           <p>Instagram: <a href="https://www.instagram.com/soundrealms" target="_blank">https://www.instagram.com/soundrealms</a></p>
           <p>Youtube: <a href="https://www.youtube.com/channel/UCQAIAb4OXuLgjnzfsprtZLQ" target="_blank">https://www.youtube.com/channel/UCQAIAb4OXuLgjnzfsprtZLQ</a></p>
           <p>Commercial video clip: <a href="https://youtu.be/cgIQ1PiHwmo" target="_blank">https://youtu.be/cgIQ1PiHwmo</a></p>
-          <p>Intro Demo Adventure: <a href="https://youtu.be/FynUSGlu4Nc" target="_blank">https://youtu.be/FynUSGlu4Nc</a></p>
-          <p>Intro The Mystery of the Silent Islands: <a href="https://youtu.be/H_xQei4UuCE" target="_blank">https://youtu.be/H_xQei4UuCE</a></p>
+          <p>Intro to "Vemhir's Castle" (Demo Adventure): <a href="https://youtu.be/FynUSGlu4Nc" target="_blank">https://youtu.be/FynUSGlu4Nc</a></p>
+          <p>Intro "The Mystery of the Silent Islands": <a href="https://youtu.be/H_xQei4UuCE" target="_blank">https://youtu.be/H_xQei4UuCE</a></p>
           <p>Binaural 360 surround demo: <a href="https://youtu.be/XO7zIgHjQzM" target="_blank">https://youtu.be/XO7zIgHjQzM</a></p>
 
           <p>
@@ -42,6 +46,9 @@
               target="_blank"
             >https://www.drivethrurpg.com/product/385247/Sound-Realms-Mace--Magic-Demo-Adventure-Companion-PDF</a>
           </p>
+
+          <h1>Contact</h1>
+          <p>Feel free to contact us at: <a href="mailto:dm@varghand.com">dm@varghand.com</a></p>
         </div>
       </section>
 
