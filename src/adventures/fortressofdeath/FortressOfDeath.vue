a41388884bf2398780dda7ea76caf4ea193a9e4e
Sundin <sundin666@gmail.com>
2023-07-15 16:58:41 +0200

    New content on FoD page (#8)

--- src/adventures/fortressofdeath/FortressOfDeath.vue
+++ src/adventures/fortressofdeath/FortressOfDeath.vue
@@ -3,12 +3,45 @@
     <div class="content">
       <TopMenu />
       <div class="section-content">
-        <h1>The Fortress of Death</h1>
+        <img src="../../assets/fod/fod-logo.png" alt="Lone Wolf: Fortress of Death" class="image" />
 
-        <img src="../../assets/fortressofdeath_info_2.jpg" class="image" />
-        <a href="https://www.kickstarter.com/projects/varghand/fortress-of-death" target="_blank">
-          <img src="../../assets/fod_kickstarter.jpg" class="image" />
-        </a>
+        <div class="text">
+          <p>
+            In 1989 the creator of the Lone Wolf adventure book series, Joe Dever, presented a
+            ground breaking new kind of interactive game called <i>PhoneQuest</i>. To play the game,
+            you dialed a number on your landline telephone. A narrative voice presented a story with
+            a selection of choices for you. All of this was accompanied by cinematic music and
+            immersive sound effects. One of these adventures was called <i>Fortress of Doom</i>.
+          </p>
+          <p>
+            Now in 2023 the time has come to reenvision Joe Dever's original concept and release a
+            game for smartphones and tablets – a new game incorporating 360 surround sound and the
+            latest app technology but still faithful to the original. This new interactive adventure
+            returns to the glory days of Lone Wolf and fantasy roleplaying games, making it the most
+            immersive audio game experience ever.
+          </p>
+          <p>
+            Enter, if you dare, <i><span class="bold">The Fortress of Death!</span></i>
+          </p>
+        </div>
+        <img src="../../assets/fod/death-or-glory.png" class="image" />
+      </div>
+      <div class="kickstarter-strip">
+        <div class="section-content inverted">
+        <p>
+            Want to help us make <i>The Fortress of Death</i> into reality? Please sign up at our
+            <a
+              href="https://www.kickstarter.com/projects/varghand/fortress-of-death"
+              target="_blank">
+              <span class="bold">Kickstarter page</span>
+            </a>
+            and help us spread the word around. Thank you, it means a lot to us!
+          </p>
+            <MyButton external=true href="https://www.kickstarter.com/projects/varghand/fortress-of-death">Join us on Kickstarter!</MyButton>
+        </div>
+      <a href="https://www.kickstarter.com/projects/varghand/fortress-of-death" target="_blank">
+        <img src="../../assets/fod/fod_kickstarter.jpg" class="image" />
+      </a>
       </div>
       <div class="strip">
         <h2>Sign up for the Newsletter</h2>
@@ -18,7 +51,7 @@
             adventures. Occassionally you will also receive free in-game bonus items and other
             goodies.
           </p>
-          <NewsletterSignupForm/>
+          <NewsletterSignupForm />
         </div>
       </div>
       <MainFooter />
@@ -31,6 +64,7 @@ import MainLayout from '../../layouts/Main.vue';
 import MainFooter from '../../components/MainFooter.vue';
 import TopMenu from '../../components/TopMenu.vue';
 import NewsletterSignupForm from '../../components/NewsletterSignupForm.vue';
+import MyButton from '../../components/MyButton.vue';
 
 export default {
   name: 'FortressOfDeath',
@@ -39,6 +73,7 @@ export default {
     MainFooter,
     TopMenu,
     NewsletterSignupForm,
+    MyButton,
   },
 };
 </script>
@@ -58,6 +93,9 @@ export default {
   justify-content: center;
   align-items: center;
 }
+.text {
+  align-items: flex-start;
+}
 .logo {
   padding: 12px;
   max-width: 60%;
@@ -66,7 +104,7 @@ export default {
 }
 .image {
   max-height: 800px;
-  padding-bottom: 100px;
+  padding: 20px;
 }
 
 h1 {
@@ -82,7 +120,17 @@ h1 {
   height: 600px;
 }
 
-.strip > h1,h2 {
+.kickstarter-strip {
+  background-color: #4a443c;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 25px;
+}
+
+.strip > h1,
+h2 {
   color: white;
 }
 </style>
