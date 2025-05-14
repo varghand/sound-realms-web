3bae5f3564461474f785197aa61a840280711dd0
Gustav Sundin <sundin666@gmail.com>
2025-05-14 13:24:51 +0200

    Add Bitly links

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -15,7 +15,7 @@
         <div class="link-col">
           <p>SOUND REALMS</p>
           <a href="/download">DOWNLOAD</a>
-          <a href="https://shop.soundrealms.com/">WEB SHOP</a>
+          <a href="https://rlms.cc/3ZaD0mk">WEB SHOP</a>
           <a href="/games">OUR GAMES</a>
           <a
             href="https://www.kickstarter.com/discover/advanced?ref=nav_search&term=sound%20realms"
